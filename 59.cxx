a70cdc2debdeb725489ef9dcc55346d8a3fd5efa
laasyasree <laasyasreesapa@gmail.com>
2019-04-28 11:59:31 +0530

    Add files via upload

---
+++ 59.cxx
@@ -0,0 +1,45 @@
+/*
+ * 59.cxx
+ * 
+ * Copyright 2019 hp <hp@hp-HP-ProBook-4530s>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{	int i,c=0;
+	string s;
+	cin >> s;
+	for(i=0;i<s.length();i++)
+		if(s[i]>64 && s[i]< 91)
+			c++;
+	if(c > (s.length()-c))
+		for(i=0;i<s.length();i++)
+			s[i]=toupper(s[i]);
+	else 
+		for(i=0;i<s.length();i++)
+			s[i]=tolower(s[i]);
+	cout << s;
+			
+	return 0;
+}
+
